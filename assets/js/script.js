4348e6737937470af30145be5b377fbad121b97f
choyoon88 <azureluster@gmail.com>
2023-02-20 18:36:35 +0000

    start writing js with the mentor

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function startGame() {
+    console.log('Start game');
+    const frontImage = document.getElementById('front-image');
+    console.log(frontImage);
+    //change the gameplay image
+    //show the question
+    //show answer options
+}
