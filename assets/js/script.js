27888a39db95d58ebe3ec327bc9ebc8e0266e175
choyoon88 <azureluster@gmail.com>
2023-02-21 21:39:01 +0000

    add question, answer and correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,73 @@
-function startGame() {
-    console.log('Start game');
-    const frontImage = document.getElementById('front-image');
-    console.log(frontImage);
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type') === 'start') {
+                alert('You clicked START');
+            } else {
+                let btn = this.getAttribute('data-type');
+                alert(`You clicked ${btn}`);
+            }
+        })
+    }
+
+})
+
+let myQuestion = [{
+        question: `What is the name of Ellie's mother?`,
+        answers: {
+            1: 'Sarah',
+            2: 'Anna',
+            3: 'Michela',
+            4: 'Mia'
+        },
+        correctAnswer: '2'
+    },
+    {
+        question: 'What animal constantly pops up in the background throughout the first game?',
+        answers: {
+            1: 'Wolf',
+            2: 'Rabbit',
+            3: 'Horse',
+            4: 'Giraffe'
+        },
+        correctAnswer: '4'
+    },
+    {
+        question: 'What item does Ellie keep of Sam\'s that can be seen in her room at the start of The Last of Us Part II?',
+        answers: {
+            1: 'PS3', 
+            2: 'Cassette Player',
+            3: 'Dinosaurs book',
+            4: 'Toy robot'
+        },
+        correctAnswer: '4'
+    },
+    {
+        question: 'Which of these buttons is NOT on Ellie\'s original backpack?',
+        answers: {
+            1: 'Giraffe sticker',
+            2: 'Flag pin',
+            3: 'Nuclear waste symbol',
+            4: 'One-eyed monster plushie'
+        },
+        correctAnswer: '1'
+    }, 
+    {
+        question: 'In the photograph of Joel and his daughter Sarah, what football shirt is she wearing?',
+        answers: {
+            1: 'England', 
+            2: 'Brazil',
+            3: 'Argentina',
+            4: 'Netherland'
+        },
+        correctAnswer: '3'
+    }
+]
+
+function startGame(question, answers) {
+
     //change the image related to the first question
     //display the question
     //display answer options
