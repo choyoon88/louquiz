32251dd7e87c3ce0f20dae8448c9541bf26af84e
choyoon88 <azureluster@gmail.com>
2023-02-27 19:02:33 +0000

    add some images on dom and figure out how to show the image and the explanation of the answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,14 @@
 const myQuestion = [{
         question: `What is the name of Ellie's mother?`,
         answers: [
-            { text: 'Sarah', correct: true },
-            { text: 'Anna', correct: false },
+            { text: 'Sarah', correct: false },
+            { text: 'Anna', correct: true },
             { text: 'Michela', correct: false },
             { text: 'Mia', correct: false}
-        ]
+        ], 
+        image: document.getElementsByClassName('anna'), 
+        explain: `While we never meet, Ellie's mother leaves a letter to Ellie before she dies and in the letter we
+        learn her name is Anna.`    
     },
     {
         question: 'What animal constantly pops up in the background throughout the first game?',
@@ -30,7 +33,11 @@ const myQuestion = [{
             {text: 'Rabbit', correct: false}, 
             {text: 'Horse', correct: false},
             {text: 'Giraffe', correct: true}
-        ]
+        ], 
+        image: document.getElementsByClassName('giraffe'), 
+        explain: `From Sarah's room to the movie theater called Giraffe Park, giraffes are all over the first game in
+        the series. All of this leads up to the beautiful scene where Ellie meets one
+        for the first time.`
     },
     {
         question: 'What item does Ellie keep of Sam\'s that can be seen in her room at the start of The Last of Us Part II?',
@@ -40,6 +47,9 @@ const myQuestion = [{
             {text: 'Dinosaurs book', correct: false},
             {text: 'Toy robot', correct: true}
         ],
+        image: document.getElementsByClassName('toy'),
+        explain: `Ellie takes the toy robot from a toy shop when Sam was told to get rid of that by his brother Henry.
+        Ellie keeps the toy robot and it can be seen in Ellie's room at the start of the second game.`
     },
     {
         question: 'Which of these buttons is NOT on Ellie\'s original backpack?',
@@ -48,7 +58,9 @@ const myQuestion = [{
             {text: 'Flag pin', correct: false},
             {text: 'Nuclear waste symbol', correct: false}, 
             {text: 'One-eyed monster plushie', correct: false}
-        ]
+        ], 
+        image: document.getElementsByClassName('sticker'), 
+        explain: `Ellie has various buttons on her backpack, but Giraffe sticker is not one of them.`
     }, 
     {
         question: 'In the photograph of Joel and his daughter Sarah, what football shirt is she wearing?',
@@ -58,6 +70,9 @@ const myQuestion = [{
             {text: 'Argentina', correct: true},
             {text: 'Netherland', correct: false}
         ],
+        image: document.getElementsByClassName('argentina'), 
+        explain: `Sarah is wearing an Argentina football shirt, which is a nod to the game's composer
+        Gustavo Santaolalla who is from Argentina.`
     }
 ];
 
@@ -69,6 +84,9 @@ const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 
+const imageEl = document.getElementById('answer-image');
+const explantionEl = document.getElementById('explanation');
+
 let shuffledQuestion, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
@@ -79,34 +97,17 @@ nextButton.addEventListener('click', () => {
 
 
 function startGame() {
-    //display the question
-    
+       
     startButton.classList.add('hidden');
     shuffledQuestion = myQuestion.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hidden');
     nextGame()
-
-    
-    // for(i = 0; i < myQuestion.length; i++) {
-        
-    //     let questionTitle = document.getElementsByClassName('question-area').innerHTML; 
-    //     questionTitle.textContent = myQuestion[i].question;
-            
-    // //display answer options
-    //     let questionAnswers = document.getElementsByClassName('answer-area').innerHTML;
-    //     questionAnswers.textContent = myQuestion[i].answers;
-
-    // //change the image related to the first question
-    // }
 }
 
 function nextGame() {
     resetState();
     showQuestion(shuffledQuestion[currentQuestionIndex]);
-    //display to the next image for the question
-    //display the next question
-    //display the next select options
 }
 
 function showQuestion(myQuestion) {
@@ -127,8 +128,7 @@ function resetState() {
     nextButton.classList.add('hidden');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-    
+    }    
 }
 
 function selectAnswer(e) {
@@ -144,29 +144,23 @@ function selectAnswer(e) {
         startButton.innerText = 'Restart';
         startButton.classList.remove('hidden');
     }
+    button.addEventListener('click', imageEl.src = myQuestion.image)    
 }
 
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
         element.classList.add('correct');
-    } else {
+        
+    } 
+    else {
         element.classList.add('wrong');
     }
 }
 
 function clearStatusClass(element) {
     element.classList.remove('correct');
-    element.classList.add('wrong');
-}
-
-function incorrecAnswer() {
-    //change to the answer image
-    //display red x-marked icon
-    //display text Wrong!
-    //display explanation to the answer
-    //display the incorrect selected answer with red text color
-    //display the correct answer option with green text color
+    element.classList.remove('wrong');
 }
 
 function incrementScore() {
